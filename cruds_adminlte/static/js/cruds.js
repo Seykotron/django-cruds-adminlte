1f648881eb48464443347b3e1af37713e28b0b41
Miguel Angel Romero de los Llanos <romerodelosllanos@mineryreport.com>
2025-09-17 10:33:18 +0200

    Update cruds.js

--- cruds_adminlte/static/js/cruds.js
+++ cruds_adminlte/static/js/cruds.js
@@ -1,7 +1,10 @@
-$(function(){
-      $('select').select2();
+$(function () {
+  $('select').select2();
+  // Verificar si iCheck está disponible antes de usarlo
+  if ($.fn.iCheck) {
       $('input').iCheck({
-        checkboxClass: 'icheckbox_minimal-blue',
-        radioClass: 'iradio_minimal-blue',
+          checkboxClass: 'icheckbox_minimal-blue',
+          radioClass: 'iradio_minimal-blue',
       });
+  }
 });
